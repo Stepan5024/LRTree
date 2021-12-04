02e5619495f770dbcde6c183403cf0bc57b5ed22
Степан <bokarevstepan1@yandex.ru>
2021-12-04 20:33:03 +0300

    Добавьте файлы проекта.

---
+++ LRTree/LRTree.cpp
@@ -0,0 +1,10 @@
+﻿// LRTree.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
